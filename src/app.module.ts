04d37b9dbd0747a2d521954ee52aab5869e7a9c5
ZakariaTalhami <zakaria.a.a.talhami@gmail.com>
2023-03-09 15:41:22 +0200

    feat: add mongoose database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_STRING)
+  ],
 })
 export class AppModule {}
