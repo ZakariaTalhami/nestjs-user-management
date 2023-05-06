9193f5cbc5d0c7f6551eef8694180afa0c91e76f
ZakariaTalhami <zakaria.a.a.talhami@gmail.com>
2023-05-06 17:18:14 +0300

    feat: sendgrid integration and email service

---
+++ src/common/email.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { MailService } from '@sendgrid/mail';
+
+@Injectable()
+export class EmailService {
+    private emailClient: MailService;
+
+    constructor() {
+      this.emailClient = new MailService();
+      this.emailClient.setApiKey(process.env.EMAIL_SERVICE_API_KEY)
+    }
+
+    async sendEmailTemplate(templateId: string, toEmail: string, data: any) {
+        console.log({
+            from: 'zakaria.a.a.talhami@gmail.com',
+            to: toEmail,
+            dynamicTemplateData: data,
+            templateId,
+        });
+        return this.emailClient.send({
+            from: 'zakaria.a.a.talhami@gmail.com',
+            to: toEmail,
+            dynamicTemplateData: data,
+            templateId,
+        });
+    }
+}
